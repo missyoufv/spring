df1227e164d4dc7d54b7bfec123b1e4c90da20e0
admin <88888888>
2018-01-05 11:56:41 +0800

    测试

--- Blog/src/main/java/cn/duw/Blog/BlogApplication.java
+++ Blog/src/main/java/cn/duw/Blog/BlogApplication.java
@@ -2,7 +2,6 @@ package cn.duw.Blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
